0853edae22995a9b4a099a5a83b69c59991f4e4c
Saint Wish <SaintWish@users.noreply.github.com>
2021-10-16 00:18:35 -0600

    added APIKey middleware

---
+++ src/Middleware/ApiKey.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace MSNexus.Middleware
+{
+    public class ApiKey
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        private readonly string _apiKey;
+
+        public ApiKey(RequestDelegate next, ILogger logger, string apiKey)
+        {
+            _next = next;
+            _logger = logger;
+            _apiKey = apiKey;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var ip = context.Connection.RemoteIpAddress;
+
+            if (!context.Request.Headers.TryGetValue(_apiKey, out var providedApiKey))
+            {
+                _logger.LogInformation($"Invalid API key from {ip}");
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return;
+            }
+
+            if (!_apiKey.Equals(providedApiKey))
+            {
+                _logger.LogInformation($"Invalid API key from {ip} using {_apiKey}");
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return;
+            }
+
+            //passed
+            await _next.Invoke(context);
+        }
+    }
+}
